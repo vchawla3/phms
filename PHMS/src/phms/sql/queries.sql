dabcad7fe711af060f0cdd6a12c2dae46c2d35b2
vchawla3 <vchawla3@ncsu.edu>
2016-10-24 14:42:39 -0400

    wrote one query

--- PHMS/src/phms/sql/queries.sql
+++ PHMS/src/phms/sql/queries.sql
@@ -1,1 +1,8 @@
 -- Add queries required for the application
+
+--Find patients who have two health supporters--
+SELECT * FROM Person p, PATIENT pa
+WHERE p.Per_Id = pa.Pat_Person
+	AND 2 = (SELECT COUNT(*) FROM Health_Supporter h
+			WHERE h.HS_Patient = pa.Pat_Person);
+			
